021cd5f1ffe80c4f81a690444629b4bb0ddba663
Oliver Banham <oliverbanham@gmail.com>
2015-04-27 23:41:30 +0100

    version 1

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Olly and Georgina's Relationship is pretty awesome</title>
+	<link rel="stylesheet" href="stylesheets/styles.css">
+	<script src="//use.typekit.net/srr8sty.js"></script>
+	<script>try{Typekit.load();}catch(e){}</script>
+</head>
+<body>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h1 class="text-center">Olly and Georgina's Relationship is pretty awesome</h1>
+		</div>
+	</div>
+	<div class="row push-down">
+		<div class="col-md-8 col-md-offset-2">
+			<img class="hero img-responsive" src="images/hero_Cover.png" alt="hero">
+		</div>
+		<div class="col-md-2"></div>
+	</div>
+
+
+	<div class="row push-dowwn">
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og1.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og2.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og3.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og4.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+	</div>
+
+
+	<div class="row push-dowwn">
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og5.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og6.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og7.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og8.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+	</div>
+
+		<div class="row push-dowwn">
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og9.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og10.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og11.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og12.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+	</div>
+
+
+	<div class="row push-dowwn">
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og13.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og14.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og15.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og16.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+	</div>
+
+		<div class="row push-dowwn">
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og17.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og18.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og19.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og20.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+	</div>
+
+
+	<div class="row push-dowwn">
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og21.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og22.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og23.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame-flip.png" alt="">	
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="frame">
+				<img class="frame-image" src="images/og24.jpg" alt="">
+				<img class="frame-pos" src="images/small-frame.png" alt="">	
+			</div>
+		</div>
+	</div>
+</div>
+
+	
+	
+	
+</body>
+</html>
